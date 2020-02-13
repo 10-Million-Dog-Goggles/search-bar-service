5e0e3c1cdc1ebe913c22b80165f5b93fd4c0439a
Adam <gienappa@gmail.com>
2020-02-13 12:17:28 -0800

    refactored createSampleData.js to utilize 'fs' module:
    can now use 'drain' to allow creation of 10M entries

--- database-benchmark/mongoDB/resources/createSampleData.js
+++ database-benchmark/mongoDB/resources/createSampleData.js
@@ -1,30 +1,32 @@
-const createCsvWriter = require('csv-writer').createObjectCsvWriter;
+const fs = require('fs');
 const faker = require('faker');
-// const mongoose = require('mongoose');
-// const Search = require('../index.js');
 
-// const headers = Object.keys(Search.schema.paths)
-//   .filter( x => ['_id', '__v'].indexOf(x) === -1);
 
-const csvWriter = createCsvWriter({
-  path: 'database-benchmark/mongoDB/resources/sampleDataM.csv',
-  header: [
-    {id: 'id', title: 'id'},
-    {id: 'term', title: 'term'},
-  ],
+const writeEntries = fs.createWriteStream(__dirname + '/sampleDataM.csv');
+writeEntries.write('id,term\n', 'utf8');
+
+const writeTenMillionEntries = (writer, encoding, callback) => {
+  let i = 10000000;
+  let id = 0;
+  write = () => {
+    let ok = true;
+    do {
+      i -= 1;
+      id += 1;
+      const data = `${faker.commerce.productMaterial()} ${faker.commerce.product()}`;
+      if (i === 0) {
+        writer.write(data, encoding, callback);
+      } else {
+        ok = writer.write(data, encoding);
+      }
+    } while (i > i && ok);
+    if (i > 0) {
+      write.once('drain', write);
+    }
+  };
+  write();
+};
+
+writeTenMillionEntries(writeEntries, 'utf8', () => {
+  writeEntries.end();
 });
-
-const data = [];
-
-for (var i = 0; i <= 10000000; i++) {
-  data.push({ id: `${i}`, term: `${faker.commerce.productMaterial()} ${faker.commerce.product()}` });
-}
-
-csvWriter
-  .writeRecords(data)
-  .then(() => console.log('csv created successfully!'))
-  .catch((err) => console.error(err));
-
-// mongoose.connection.close()
-//   .then( () => console.log('mongoose disconnected') )
-//   .catch( err => console.error(err) );
