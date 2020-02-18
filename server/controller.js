fcd59de74d2fdc0bad7537baf6bccbf7ebfcbb14
Adam <gienappa@gmail.com>
2020-02-18 11:13:06 -0800

    created postgres controller:   - added to current server controller file   - renamed mongo controller method for easy switching

--- server/controller.js
+++ server/controller.js
@@ -1,14 +1,34 @@
-const model = require('../database-benchmark/mongoDB/model.js');
+const modelM = require('../database-benchmark/mongoDB/model.js');
+const modelP = require('../database-benchmark/postgreSQL/model.js');
 
+//mongoose controller:
 module.exports = {
-  getAll: (req, res) => {
+  getAllM: (req, res) => {
     let regexString = '';
     for (var i = 0; i < req.query.term.length; i++) {
       regexString += '.*' + req.query.term[i];
     }
     regexString += '.*';
-    model.autocomplete(regexString)
+    modelM.getResults(regexString)
       .then((items) => res.status(200).send(items))
       .catch(err => res.status(400).send(err));
+  }
+};
+
+//postgres controller:
+module.exports = {
+  getAllP: (req, res) => {
+    let regexString = '';
+    for (var i = 0; i < req.query.term.length; i++) {
+      regexString += '%' + req.query.term[i];
+    }
+    regexString += '%';
+    modelP.getResults({ regexString }, (err, data) => {
+      if (err) {
+        res.status(400).send(err);
+      } else {
+        res.status(200).send(data);
+      }
+    });
   }
 };
