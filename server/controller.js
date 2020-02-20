710dfaeb9cd274249c5c5450dc048b8aa28e6066
Adam <gienappa@gmail.com>
2020-02-20 10:14:32 -0800

    psql refactored to use GIN indexing (was B-tree)

--- server/controller.js
+++ server/controller.js
@@ -1,19 +1,19 @@
-const modelM = require('../database-benchmark/mongoDB/model.js');
+// const modelM = require('../database-benchmark/mongoDB/model.js');
 const modelP = require('../database-benchmark/postgreSQL/model.js');
 
 
 module.exports = {
   //mongoose controller:
-  getAllM: (req, res) => {
-    let regexString = '';
-    for (var i = 0; i < req.query.term.length; i++) {
-      regexString += '.*' + req.query.term[i];
-    }
-    regexString += '.*';
-    modelM.getResults(regexString)
-      .then((items) => res.status(200).send(items))
-      .catch(err => res.status(400).send(err));
-  },
+  // getAllM: (req, res) => {
+  //   let regexString = '';
+  //   for (var i = 0; i < req.query.term.length; i++) {
+  //     regexString += '.*' + req.query.term[i];
+  //   }
+  //   regexString += '.*';
+  //   modelM.getResults(regexString)
+  //     .then((items) => res.status(200).send(items))
+  //     .catch(err => res.status(400).send(err));
+  // },
   //postgres controller:
   getAllP: (req, res) => {
     let regexString = '';
--- spec/database.test.js
+++ spec/database.test.js
@@ -1,45 +1,36 @@
-const mongoose = require('mongoose');
-mongoose.Promise = global.Promise;
-const models = require('../database/models.js');
+const modelM = require('../database-benchmark/mongoDB/model.js');
+const modelP = require('../database-benchmark/postgreSQL/model.js');
 
 describe('database', () => {
 
   // importing models will actually create the connection for us!
   // just make sure we dump everything before we start!
-  beforeAll(async (done) => {
-    await mongoose.connection.collections['searchitems'].deleteMany();
-    done();
-  });
+  // beforeAll(async (done) => {
+  //   await mongoose.connection.collections['searchitems'].deleteMany();
+  //   done();
+  // });
 
-  afterAll(async (done) => {
-    await mongoose.connection.close(true)
-    done();
-  });
+  // afterAll(async (done) => {
+  //   await mongoose.connection.close(true)
+  //   done();
+  // });
 
-  test ('should get all the items', (done) => {
-    var sampleSearches = [{ id: 1, name: "testItem1" }, { id: 2, name: "testItem2" }, { id: 3, name: "testItem3" }];
-    return models.addItems(sampleSearches)
-      .then(() => {
-        return models.getAll();
-      })
-      .then(dbResponse => {
-        expect(dbResponse.length).toBe(3);
-        done();
-      });
+  // test ('should get all the items', (done) => {
+  //   var sampleSearches = [{ id: 1, name: "testItem1" }, { id: 2, name: "testItem2" }, { id: 3, name: "testItem3" }];
+  //   return models.addItems(sampleSearches)
+  //     .then(() => {
+  //       return models.getAll();
+  //     })
+  //     .then(dbResponse => {
+  //       expect(dbResponse.length).toBe(3);
+  //       done();
+  //     });
+  // });
+
+  test ('MongoDB should retrieve results in less than 50ms', () => {
+    const testStrings = ['.*a.*u.*t.*', '.*b.*a.*r.*t.*', '.*l.*i.*d.*e.*r.*o.*', '.*t.*e.*s.*t.*'];
+    
   });
   
-  test ('adds a search entry', (done) => {
-    return models.addItems({id: 1000, name: "test_item"})
-    .then(() => {
-      return models.getAll();
-    })
-    .then(dbResponse => {
-      expect(dbResponse[0]).toEqual(expect.objectContaining({
-        id: expect.any(Number),
-        name: expect.any(String)
-      }));
-      done();
-    });
-  });
 });
 
