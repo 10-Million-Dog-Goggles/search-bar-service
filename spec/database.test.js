adfd5c0da43d54aa6f334268efe988b581cc0921
Adam <gienappa@gmail.com>
2020-02-20 16:16:33 -0800

    postgres tests added to test file.

--- spec/database.test.js
+++ spec/database.test.js
@@ -1,41 +1,56 @@
 const modelM = require('../database-benchmark/mongoDB/model.js');
-const modelP = require('../database-benchmark/postgreSQL/model.js');
 
-describe('database', () => {
 
-  // importing models will actually create the connection for us!
-  // just make sure we dump everything before we start!
-  // beforeAll(async (done) => {
-  //   await mongoose.connection.collections['searchitems'].deleteMany();
-  //   done();
-  // });
-
-  // afterAll(async (done) => {
-  //   await mongoose.connection.close(true)
-  //   done();
-  // });
-
-  // test ('should get all the items', (done) => {
-  //   var sampleSearches = [{ id: 1, name: "testItem1" }, { id: 2, name: "testItem2" }, { id: 3, name: "testItem3" }];
-  //   return models.addItems(sampleSearches)
-  //     .then(() => {
-  //       return models.getAll();
-  //     })
-  //     .then(dbResponse => {
-  //       expect(dbResponse.length).toBe(3);
-  //       done();
-  //     });
-  // });
+describe('MongoDB performance', () => {
 
   test ('MongoDB should retrieve results in less than 50ms', (done) => {
-    const testStrings = ['.*a.*u.*t.*', '.*b.*a.*r.*t.*', '.*l.*i.*d.*e.*r.*o.*', '.*t.*e.*s.*t.*'];
-    const queryArray = testStrings.map(str => queryArray.push(modelM.getResults(str).explain('executionStats')));
+    const testStrings = ['.*a.*u.*t.*', '.*b.*a.*r.*t.*', '.*l.*i.*d.*e.*r.*o.*', '.*o.*m.*n.*i.*s.*p.*e.*r.*f.*e.*r.*e.*n.*d.*i.*s.*'];
+    const queryArray = testStrings.map((str) => modelM.getResults(str).explain('executionStats'));
+    
+    const statsArray = [];
     Promise.all(queryArray)
-      .then(results => {
-        console.log(results);
+      .then((results) => {
+        results.forEach(res => statsArray.push(res[0].executionStats.executionTimeMillis));
+      })
+      .then(() => { 
+        statsArray.forEach((stat) => {
+          expect(stat).toBeLessThan(50);
+        });
         done();
       });
   });
   
 });
 
+const Pool = require('pg').Pool;
+const pool = new Pool({
+  user: 'gienappa',
+  host: 'localhost',
+  database: 'searchterms',
+  password: '66jtune66',
+  port: 5432,
+}, () => console.log('Postgres connected'));
+
+describe('Postgres performance', () => {
+
+  test ('Postgres should retrieve results in less than 50ms', (done) => {
+    const results = [];
+
+    const testStrings = ['.*a.*u.*t.*', '.*b.*a.*r.*t.*', '.*l.*i.*d.*e.*r.*o.*', '.*o.*m.*n.*i.*s.*p.*e.*r.*f.*e.*r.*e.*n.*d.*i.*s.*'];
+    const queryFn = (regexp) => {
+      pool.query(`EXPLAIN ANALYZE SELECT term FROM searchterms WHERE term LIKE '${regexp}' LIMIT 10`, (err, data) => {
+        if (err) {
+          console.error(err);
+        } else {
+          results.push(data);
+        }
+      });
+    }
+
+    testStrings.forEach(async (str) => {
+      await queryFn(str);
+    });
+
+    console.log(results);
+  });
+})
