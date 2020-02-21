6a7f361b11cbf2366d9fd4411fb7361e622b1097
Adam <gienappa@gmail.com>
2020-02-20 17:30:02 -0800

    db test suite complete

--- spec/database.test.js
+++ spec/database.test.js
@@ -1,5 +1,60 @@
 const modelM = require('../database-benchmark/mongoDB/model.js');
 
+const Pool = require('pg').Pool;
+const pool = new Pool({
+  user: 'gienappa',
+  host: 'localhost',
+  database: 'searchterms',
+  password: '66jtune66',
+  port: 5432,
+});
+
+
+describe('Postgres performance', () => {
+
+  const testStrings = ['%a%u%t%', '%b%a%r%t%', '%l%i%d%e%r%o%', '%o%m%n%i%s%p%e%r%f%e%r%e%n%d%i%s%'];
+
+  test ('Postgres should retrieve results in less than 50ms', async (done) => {
+    const data = await pool.query(`EXPLAIN ANALYZE SELECT term FROM searchterms WHERE term LIKE '${testStrings[0]}' LIMIT 10`);
+    
+    const execTime = Number((data.rows[data.rows.length - 1]['QUERY PLAN'].split(':')[1]).slice(0, -3));
+    // console.log(execTime);
+    expect(execTime).toBeLessThan(50);
+
+    done();
+  });
+
+  test ('Postgres should retrieve results in less than 50ms', async (done) => {
+    const data = await pool.query(`EXPLAIN ANALYZE SELECT term FROM searchterms WHERE term LIKE '${testStrings[1]}' LIMIT 10`);
+    
+    const execTime = Number((data.rows[data.rows.length - 1]['QUERY PLAN'].split(':')[1]).slice(0, -3));
+    // console.log(execTime);
+    expect(execTime).toBeLessThan(50);
+
+    done();
+  });
+
+  test ('Postgres should retrieve results in less than 50ms', async (done) => {
+    const data = await pool.query(`EXPLAIN ANALYZE SELECT term FROM searchterms WHERE term LIKE '${testStrings[2]}' LIMIT 10`);
+    
+    const execTime = Number((data.rows[data.rows.length - 1]['QUERY PLAN'].split(':')[1]).slice(0, -3));
+    // console.log(execTime);
+    expect(execTime).toBeLessThan(50);
+
+    done();
+  });
+
+  test ('Postgres should retrieve results in less than 50ms', async (done) => {
+    const data = await pool.query(`EXPLAIN ANALYZE SELECT term FROM searchterms WHERE term LIKE '${testStrings[3]}' LIMIT 10`);
+    
+    const execTime = Number((data.rows[data.rows.length - 1]['QUERY PLAN'].split(':')[1]).slice(0, -3));
+    // console.log(execTime);
+    expect(execTime).toBeLessThan(50);
+
+    done();
+  });
+});
+
 
 describe('MongoDB performance', () => {
 
@@ -21,36 +76,3 @@ describe('MongoDB performance', () => {
   });
   
 });
-
-const Pool = require('pg').Pool;
-const pool = new Pool({
-  user: 'gienappa',
-  host: 'localhost',
-  database: 'searchterms',
-  password: '66jtune66',
-  port: 5432,
-}, () => console.log('Postgres connected'));
-
-describe('Postgres performance', () => {
-
-  test ('Postgres should retrieve results in less than 50ms', (done) => {
-    const results = [];
-
-    const testStrings = ['.*a.*u.*t.*', '.*b.*a.*r.*t.*', '.*l.*i.*d.*e.*r.*o.*', '.*o.*m.*n.*i.*s.*p.*e.*r.*f.*e.*r.*e.*n.*d.*i.*s.*'];
-    const queryFn = (regexp) => {
-      pool.query(`EXPLAIN ANALYZE SELECT term FROM searchterms WHERE term LIKE '${regexp}' LIMIT 10`, (err, data) => {
-        if (err) {
-          console.error(err);
-        } else {
-          results.push(data);
-        }
-      });
-    }
-
-    testStrings.forEach(async (str) => {
-      await queryFn(str);
-    });
-
-    console.log(results);
-  });
-})
