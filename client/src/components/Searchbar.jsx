26b704a0d0352967cc54c3276719c10b66fdc958
Adam <gienappa@gmail.com>
2020-02-29 14:04:36 -0800

    refactored client to work with revised db queries
    search bar api calls debounced to minimize query freq.

--- client/src/components/Searchbar.jsx
+++ client/src/components/Searchbar.jsx
@@ -7,25 +7,11 @@ class SearchBar extends React.Component {
     this.state = {
       searchVisibility: false,
       autoFills: [],
-      matchingFillsIndex: [],
-      matchingFillsFormatted: []
     };
+    this.timer;
     this.onChangeHandler = this.onChangeHandler.bind(this);
     this.onBlurHandler = this.onBlurHandler.bind(this);
     this.onFocusHandler = this.onFocusHandler.bind(this);
-  }
-
-// on mount, get all the potential autofills
-  componentDidMount() {
-    axios
-      .get('http://localhost:3001/api')
-      // .get('/api')
-      .then(autoFills => {
-        this.setState({
-          autoFills: autoFills.data
-        });
-      })
-      .catch(err => console.error(err));
   }
 
   onBlurHandler() {
@@ -41,91 +27,33 @@ class SearchBar extends React.Component {
   }
 
   // on change of the search field
-  // filter the autoFill list for anything that contains the substring
+  // grab results from db
   onChangeHandler(e) {
     const searchTerm = e.target.value;
     if (searchTerm === '') {
       this.setState({
-        matchingFillsIndex: [],
-        matchingFillsFormatted: [],
         searchVisibility: false
       });
       return;
     }
 
-    let regexString = '';
+    const apiCall = (string) => {
+      axios.get(`http://localhost:3001/api/search/?term=${string}`)
+        .then((data) => {
+          this.setState({
+            autoFills: data.data,
+            searchVisibility: true
+          });
+        })
+        .catch((err) => console.error(err));
+    };
 
-    // this loop builds a regEx expression that ignores whitespace between all characters
-    // \s* is a regex term that ignores whitespace, use a \\ to escape the second slash because strings
-    for (var i = 0; i < searchTerm.length; i++) {
-      regexString += '\\s*' + searchTerm[i];
-    }
-    // add a regex value for spaces after as well
-    regexString += '\\s*';
+    const debounce = (fn, input, delay) => {
+      clearTimeout(this.timer);
+      this.timer = setTimeout(() => fn.call(null, input), delay);
+    };
 
-    // create a regexQuery object to feed into the string
-    let regexQuery = new RegExp(regexString);
-    let matchingFillsIndex = [];
-    let matchingFillsFormatted = [];
-
-    this.state.autoFills.forEach((autoFillItem, index) => {
-      let autoFillTerm = autoFillItem.name;
-      // search within the all the autofill options
-      let indexOfSearchStart = autoFillTerm.search(regexQuery);
-
-      // the case where we do find the searchterm
-      if (indexOfSearchStart !== -1) {
-        matchingFillsIndex.push(index);
-        let indexOfSearchEnd = this.findEndIndexIgnoreSpaces(searchTerm, autoFillTerm, indexOfSearchStart);
-
-        // now we need to add spans before and after the search term
-        let returnValue = autoFillTerm;
-        returnValue = this.stringSpliceAdd(returnValue, indexOfSearchEnd + 1, '</span>');
-        returnValue = this.stringSpliceAdd(returnValue, indexOfSearchStart, '<span class="AJautofillhighlight">');
-
-        matchingFillsFormatted.push(returnValue);
-
-      }
-    }
-    );
-
-    this.setState({
-      matchingFillsIndex,
-      matchingFillsFormatted,
-      searchVisibility: true
-    });
-  }
-
-  /*
-    search term is the string that we're looking for
-    searchIn is the string we're searching within
-    startingIndex is the index where we know the searchTerm starts
-    
-    We're looking for the ending index of the searchTerm within searchIn
-      ignoring all whitespaces inbetween each letter of the searchTerm
-
-    return: the index of the last space
-  */
-  findEndIndexIgnoreSpaces(searchTerm, searchIn, startingIndex) {
-    for (var i = startingIndex, j = 0; i < searchIn.length; i++) {
-      // check to see if the current letter within the search term equals the letter in the search term
-      if (searchIn[i] === searchTerm[j]) {
-        j++;
-        // if J is now equal to the length of the searchTerm, then 
-        if (j === searchTerm.length) {
-          return i;
-        }
-      }
-    }
-
-    return -1;
-  }
-
-  // str is the string to operate on
-  // index is where to add the string
-  // add is the string to add
-  stringSpliceAdd(str, index, add) {
-    return str.slice(0, index) + add + str.slice(index, str.length);
+    debounce(apiCall, searchTerm, 200);
   }
 
   render() {
@@ -139,18 +67,14 @@ class SearchBar extends React.Component {
             </svg>
           </button>
           {(this.state.searchVisibility ? 
-          <div className="AJautofillmodal">
-            <ul className="AJautofilllist">
-            {
-              this.state.matchingFillsFormatted.map((item, index) => {
-                if (index < 10) {
-                  return <li className="AJautofillitems" dangerouslySetInnerHTML={{__html: item}}></li>;
-                }
-              })
-            }
-            </ul>
-          </div>
-          : null)}
+            <div className="AJautofillmodal">
+              <ul className="AJautofilllist">
+                {this.state.autoFills.map((item, index) => {
+                  return <li className="AJautofillitems" dangerouslySetInnerHTML={{__html: item.term}}></li>;
+                })}
+              </ul>
+            </div>
+            : null)}
         </form>
       </div>
     );
